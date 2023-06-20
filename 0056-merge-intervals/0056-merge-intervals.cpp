a07b8b7d865735c2bd479f30f60bd348b0882a21
BlackLotus454 <110842812+BlackLotus454@users.noreply.github.com>
2023-06-20 18:51:17 +0530

    Time: 41 ms (71.74%), Space: 18.9 MB (94.50%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -11,23 +11,14 @@ public:
      
         for(int i=0;i<n;i++)
         {
-            int start=intervals[i][0];
-            int end=intervals[i][1];
-            if(!ans.empty() && end<=ans.back()[1]){
-                continue;
+           
+            if(ans.empty() || intervals[i][0]>ans.back()[1]){
+                ans.push_back(intervals[i]);
+               
             }
-            for(int j=i+1;j<n;j++)
-            {
-                if(end>=intervals[j][0])
-                {
-                    end=max(end,intervals[j][1]);
-                }
-                else{
-                    break;
-                }
-       
-            }
-                 ans.push_back({start,end});
+           else{
+               ans.back()[1]=max( ans.back()[1],intervals[i][1]);
+           }
         }
         return ans;
     }
